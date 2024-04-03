35962b6da8a0e35617222b40fd974e32dc36fa71
manzo <manzo@DESKTOP-AREA6LL>
2024-04-03 17:45:06 +0200

    First commit

---
+++ BakerySystem/BakerySystem/BakerySystem/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BakerySystem
+{
+    public class MyDbContext : DbContext
+    {
+        protected override void
+        OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = 127.0.0.1; Initial Catalog = BakerySystemDatabase; User ID = sa; Trust Server Certificate = True");
+        }
+    }
+    //Db Sets come here…see later
+}
